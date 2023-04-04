3b822cce589f4827b1fddb0e2390406c0a190599
BERKAY GÜL <berrkaygul@gmail.com>
2023-04-05 01:12:49 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  const onAddTodo = () => {
+    setTodo([...todo, { text: inputValue, isCompleted: true }]);
+  };
+ 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputCol}>
+        <TextInput
+          style={styles.inputStyle}
+          onChangeText={(text) => setInputValue(text)}
+          value={inputValue}
+        />
+        <Button title="Ekle" onPress={onAddTodo} />
+      </View>
+      <View>
+        {todo.map((item, idx) => (
+          <Text key={idx}>{item.text}</Text>
+        ))}
+      </View>
     </View>
   );
 }
@@ -13,8 +32,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputStyle: {
+    backgroundColor: "#ccc",
+    padding: 7,
+    borderRadius: 10,
+    width: 100,
+    marginRight: 5,
+  },
+  inputCol: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  btn: {
+    borderRadius: 10,
+    width: 50,
   },
 });
